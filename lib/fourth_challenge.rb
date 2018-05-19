4b05c84af294564607ed49fbfbee32dc707eeedd
Alexandra Hernandez <ms.hernandeza1@gmail.com>
2018-05-19 16:38:09 +0000

    automatically backed up by learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -16,7 +16,6 @@ def fourth_challenge
         #your hashes here!
         {name: "Benvolio", age: "17", attitude: "worried"}
         {name: "Mercutio", age: "18", attitude: "hot-headed"}
-        
       ]
    }
 }
